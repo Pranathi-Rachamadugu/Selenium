41a8d1b468b3c14080c2ea5447e3511ba5ec0088
Pranathi-Rachamadugu <114640081+Pranathi-Rachamadugu@users.noreply.github.com>
2022-09-29 13:56:24 +0530

    Add files via upload
    Added file

---
+++ Demo2.java
@@ -0,0 +1,12 @@
+package javapack;
+import Introduction.X;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		X c=new X(15, 5);
+	    c.sum();
+	}
+
+}
